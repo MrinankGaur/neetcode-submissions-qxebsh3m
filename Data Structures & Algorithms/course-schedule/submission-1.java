f24b8660053ea3f3e313ea5061f1997f7a30c39d
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-04-05 16:18:51 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        int[] indeg = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge: prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            indeg[v]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0;i<n;i++){
+            if(indeg[i]==0) q.offer(i);
+        }
+        int  size = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            size++;
+            for(int next:adj.get(node)){
+                indeg[next]--;
+                if(indeg[next]==0){
+                    
+                    q.offer(next);
+                }
+            }
+        }
+        return size==n;
+
+
+    }
+}
